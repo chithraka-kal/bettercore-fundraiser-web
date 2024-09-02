f7d00c9164f9866d0fd622685c934a3c2449d350
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-02 14:02:53 +0530

    Update CampaignDetail.js

---
+++ client/src/Components/Campaign/CampaignDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CampaignDetail = () => {
+  return (
+    <div>
+      <h2>Campaign Detail</h2>
+      {}
+    </div>
+  );
+};
+
+export default CampaignDetail;

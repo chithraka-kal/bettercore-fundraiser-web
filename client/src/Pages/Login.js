b6b91f5253fd634eca60ef114f72b7cee3350a06
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-02 21:38:00 +0530

    Update Login.js

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -14,37 +14,37 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col w-full max-w-6xl overflow-hidden bg-white shadow-lg md:flex-row rounded-2xl">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <div className="flex flex-col w-full max-w-4xl h-[600px] bg-white shadow-lg md:flex-row rounded-2xl">
         {/* Left side (form) */}
-        <div className="w-full p-8 md:w-1/2">
-          <h2 className="mb-8 text-3xl font-bold text-center text-gray-800">
+        <div className="w-full p-6 md:w-1/2">
+          <h2 className="mb-4 text-2xl font-bold text-center text-gray-800">
             Sign In
           </h2>
 
-          <button className="flex items-center justify-center w-full px-4 py-3 mb-4 text-white bg-black border border-gray-300 rounded-xl hover:bg-gray-700">
-            <img src={googleLogo} alt="Google logo" className="w-6 h-6 mr-3" />
+          <button className="flex items-center justify-center w-full px-3 py-2 mb-2 text-white bg-black border border-gray-300 rounded-lg hover:bg-gray-700">
+            <img src={googleLogo} alt="Google logo" className="w-5 h-5 mr-2" />
             Sign in with Google
           </button>
 
-          <button className="flex items-center justify-center w-full px-4 py-3 mb-6 text-white bg-[#1877F2] border border-gray-300 rounded-xl hover:bg-white hover:text-black">
+          <button className="flex items-center justify-center w-full px-3 py-2 mb-3 text-white bg-[#1877F2] border border-gray-300 rounded-lg hover:bg-white hover:text-black">
             <img
               src={facebookLogo}
               alt="Facebook logo"
-              className="w-6 h-6 mr-3"
+              className="w-5 h-5 mr-2"
             />
             Sign in with Facebook
           </button>
 
-          <div className="relative mb-6 text-center">
+          <div className="relative mb-4 text-center">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300"></div>
             </div>
-            <div className="relative px-3 text-gray-500 bg-white">or</div>
+            <div className="relative px-2 text-gray-500 bg-white">or</div>
           </div>
 
           <form onSubmit={handleSubmit}>
-            <div className="mb-4">
+            <div className="mb-3">
               <label className="block text-sm font-medium text-gray-700">
                 Email
               </label>
@@ -52,12 +52,12 @@ const Login = () => {
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="block w-full px-4 py-3 mt-1 border border-gray-300 shadow-sm rounded-xl focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 placeholder="Enter your email"
                 required
               />
             </div>
-            <div className="mb-6">
+            <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700">
                 Password
               </label>
@@ -65,20 +65,20 @@ const Login = () => {
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="block w-full px-4 py-3 mt-1 border border-gray-300 shadow-sm rounded-xl focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 placeholder="Enter your password"
                 required
               />
             </div>
             <button
               type="submit"
-              className="w-full px-4 py-3 text-white bg-indigo-600 shadow-md rounded-xl hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+              className="w-full px-4 py-2 text-white bg-indigo-600 rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
             >
               Sign In
             </button>
           </form>
 
-          <div className="mt-6 text-center">
+          <div className="mt-4 text-center">
             <p className="text-sm text-gray-600">
               Don’t have an account?{" "}
               <Link

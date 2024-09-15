043632c2b52209ad67a55295aa39479624bcf3a0
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-15 23:09:53 +0530

    user model

---
+++ api/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);

c5486dab7bd15cc53085bb78fd5be3e52b86c78f
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-16 20:12:22 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import Layout from "./Components/Layout";
 import IndexPage from "./Pages/IndexPage";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import { UserContextProvider } from "./context/UserContext";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 

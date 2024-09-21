9ce71aa9af72211182d41b65347c6c9af1509767
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-22 02:12:33 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,10 +15,12 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 //importing routes
 const authRoute = require("./routes/authRoute");
 const userRoute = require("./routes/userRoute");
+const campaignRoute = require("./routes/campaignRoute");
 
 //routes
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
+app.use("/campaign", campaignRoute);
 
 mongoose.connect(
   "mongodb+srv://nipunavishka123:V1VP99fzz9BGKf7A@cluster0.kbd2l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"

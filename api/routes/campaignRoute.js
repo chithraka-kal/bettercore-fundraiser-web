7c440bc9f7c93169da4ff07a28cea444604e5e93
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-22 02:12:56 +0530

    Update campaignRoute.js

--- api/routes/campaignRoute.js
+++ api/routes/campaignRoute.js
@@ -1,8 +1,48 @@
 const express = require("express");
 const { authenticateUser } = require("../middlewares/authentication");
 const uploadMiddleware = require("../middlewares/uploadMiddleware");
+const saveFile = require("../services/fileService");
 const router = express.Router();
+const Campaign = require("../models/Campaign");
+router.post(
+  "/",
+  authenticateUser,
+  uploadMiddleware.fields([
+    { name: "img", maxCount: 1 },
+    { name: "proof", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const img = req.files['img'][0].filename;
+      const proof = req.files["proof"][0].filename;
+      const {
+        name,
+        description,
+        phone,
+        goal,
+        holder,
+        bankName,
+        accNumber,
+        swift,
+      } = req.body;
+      const campDoc = await Campaign.create({
+        name,
+        description,
+        phone,
+        goal,
+        holder,
+        bankName,
+        accNumber,
+        swift,
+        createdBy: req.user._id,
+        img,
+        proof,
+      });
+      res.status(201).json(campDoc);
+    } catch (e) {
+      res.status(400).json({ message: e.message });
+    }
+  }
+);
 
-router.post("/",authenticateUser,uploadMiddleware.array("files",2), async (req, res) => {
-
-});
+module.exports = router;

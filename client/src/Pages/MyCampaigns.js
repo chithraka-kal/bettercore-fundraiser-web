b2d9d273d73cd14749a9e914045f8feb71cdbc90
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-28 13:40:12 +0530

    add mycampaigns layout changes

--- client/src/Pages/MyCampaigns.js
+++ client/src/Pages/MyCampaigns.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function MyCampaigns() {
   const [campaigns, setCampaigns] = useState([
@@ -30,45 +31,44 @@ function MyCampaigns() {
     setCampaigns(updateCampaigns);
   };
   return (
-    <div>
-      <div className="my-10 rounded-lg shadow-lg">
-        <div>
-            <h2 className="my-5 text-xl font-semibold">My Campaign List</h2>
-          <ul className="space-y-4">
-            {campaigns && campaigns.length > 0 ? (
-              campaigns.map((campaign, index) => (
-                <li
-                  key={index}
-                  className="p-4 bg-white border rounded-lg shadow-lg"
-                >
+    <div className="my-10 px-6">
+      <div>
+        <h2 className="my-5 text-xl font-semibold">My Campaign List</h2>
+        <ul className="space-y-4">
+          {campaigns && campaigns.length > 0 ? (
+            campaigns.map((campaign, index) => (
+              <li
+                key={index}
+                className="p-4 bg-white border rounded-lg hover:shadow-lg duration-300 flex justify-between items-end"
+              >
+                <div>
                   <h3 className="text-lg font-bold">{campaign.name}</h3>
                   <p>{campaign.description}</p>
                   <p className="font-semibold">Goal: ${campaign.goal}</p>
                   <p className="text-gray-500">
                     Contact: {campaign.phoneNumber}
                   </p>
-                  <div className="flex justify-end gap-4 mt-4">
-                    <button
-                      onClick={() => editCampaign(index)}
-                      className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700"
-                    >
-                      Edit
-                    </button>
-
-                    <button
-                      onClick={() => deleteCampaign(index)}
-                      className="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-700"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </li>
-              ))
-            ) : (
-              <p>No campaigns available</p>
-            )}
-          </ul>
-        </div>
+                </div>
+                <div className="flex justify-end gap-4">
+                  <Link
+                    className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700"
+                    to={`/campaigns/my/edit/${index}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    className="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-700"
+                    to={`/campaigns/my/edit/${index}`}
+                  >
+                    Delete
+                  </Link>
+                </div>
+              </li>
+            ))
+          ) : (
+            <p>No campaigns available</p>
+          )}
+        </ul>
       </div>
     </div>
   );

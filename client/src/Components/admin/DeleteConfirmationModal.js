1610ccaeb4197b879d7a1c51d5f93caca8429583
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-30 00:16:15 +0530

    Update DeleteConfirmationModal.js

--- client/src/Components/admin/DeleteConfirmationModal.js
+++ client/src/Components/admin/DeleteConfirmationModal.js
@@ -6,10 +6,10 @@ const DeleteConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-lg font-bold text-gray-800">Confirm Deletion</h2>
+        <h2 className="text-lg font-bold text-gray-800">Confirm Block</h2>
         <p className="mt-4 text-sm text-gray-600">
-          Are you sure you want to delete this campaign? This action cannot be
-          undone.
+          Are you sure you want to Block this User or Item? This action cannot
+          be undone.
         </p>
         <div className="flex justify-end mt-6 space-x-3">
           <button
@@ -22,7 +22,7 @@ const DeleteConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
             onClick={onConfirm}
             className="px-4 py-2 font-semibold text-white bg-red-600 rounded hover:bg-red-700"
           >
-            Delete
+            Block
           </button>
         </div>
       </div>

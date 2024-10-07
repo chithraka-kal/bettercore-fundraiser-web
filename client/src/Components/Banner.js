2fd5019e5185ec3f4b38d06f6f7e96b5d5ab2919
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-07 19:51:13 +0530

    Update Banner.js

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Stat = ({ amount, description }) => (
   <div className="stat">
-    <div className="amount">{amount}</div>
+    <div className="text-4xl font-bold mb-3">{amount}</div>
     <div className="description">{description}</div>
   </div>
 );
